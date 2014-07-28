865fb5cf0582caddce4f2607177dfc07143d6df1
kumabook <kumabook@live.jp>
2014-07-28 23:33:06 +0900

    [#5] Implement models

--- Model.hs
+++ Model.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE EmptyDataDecls    #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+
 module Model where
 
 import Yesod
 import Data.Text (Text)
+import Data.Time
 import Database.Persist.Quasi
 import Data.Typeable (Typeable)
 import Prelude
 
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
