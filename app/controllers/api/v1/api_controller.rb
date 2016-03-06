14a474e891ff8b8a89333df0ab46b7ab6c6f131c
Hiroki Kumamoto <kumabook@live.jp>
2016-03-06 18:28:05 +0900

    Skip verify_authenticity_token in order to avoid `Can't verify CSRF token authenticity`

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApiController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
