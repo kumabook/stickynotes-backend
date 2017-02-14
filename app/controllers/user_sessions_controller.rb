f25645543215e0a5da763dc5aa428197b6b07dc4
Hiroki Kumamoto <kumabook@live.jp>
2017-02-14 19:18:46 +0900

    logout needs require_login action

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, except: [:destroy]
+  before_action :require_login, only: [:destroy]
   def new
     @user = User.new
   end
