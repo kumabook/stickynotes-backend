e96c9ffd6cb96063c68005a05b319ab618042b0c
Hiroki Kumamoto <kumabook@live.jp>
2017-04-02 00:30:12 +0900

    Add UsersController test

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  include Sorcery::TestHelpers::Rails::Controller
+
+  test "admin should get index" do
+    @admin = users(:admin)
+    login_user(@admin, login_url)
+    get :index
+    assert_response :success
+  end
+
+  test "member should redirect to index" do
+    @member = users(:one)
+    login_user(@member, login_url)
+    get :index
+    assert_response :redirect
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, params: {
+             user: {
+               email:                 'test@test.com',
+               password:              'password',
+               password_confirmation: 'password',
+             }
+           }
+    end
+  end
+
+  test "should show user" do
+    get :show, params: { id: users(:one).id }
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, params: { id: users(:one).id }
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, params: {
+            id: users(:one).id,
+            user: {
+              email: "new@example.com"
+            }
+          }
+    assert_redirected_to user_url(users(:one))
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, params: { id: users(:one).id }
+    end
+
+    assert_redirected_to users_url
+  end
+
+end
