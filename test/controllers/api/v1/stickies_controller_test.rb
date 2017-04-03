4ad0f2df03e7d5fea974411796453a552e337d7d
Hiroki Kumamoto <kumabook@live.jp>
2017-04-03 12:03:58 +0900

    Add Api::V1::StickiesControllerTest

---
+++ test/controllers/api/v1/stickies_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class Api::V1::StickiesControllerTest < ActionDispatch::IntegrationTest
+  include Sorcery::TestHelpers::Rails::Integration
+  include Sorcery::TestHelpers::Rails::Controller
+
+  setup do
+    @application = Doorkeeper::Application.create!(name: 'firefox',
+                                                   redirect_uri: 'urn:ietf:wg:oauth:2.0:oob')
+    @email    = 'test@test.com'
+    @password = 'test_password'
+    @user = User.create!(email: @email,
+                         password: @password,
+                         password_confirmation: @password,
+                         type: Member.name)
+    post '/oauth/token.json', params: {
+           grant_type:    'password',
+           client_id:     @application.uid,
+           client_secret: @application.secret,
+           username:      @email,
+           password:      @password
+         }
+    @token = JSON.parse @response.body
+#    @sticky = stickies(:one)
+  end
+
+  test "should get index" do
+    get '/api/v1/stickies',
+        params: {},
+        headers: {
+          Authorization: "Bearer #{@token['access_token']}",
+          CONTENT_TYPE:  "application/json",
+          ACCEPT:        "application/json",
+        }
+    assert_response :success
+  end
+
+  test "should import stickies" do
+    post '/api/v1/stickies',
+         params: {},
+         headers: {
+           Authorization: "Bearer #{@token['access_token']}",
+           CONTENT_TYPE:  "application/json",
+           ACCEPT:        "application/json",
+         }
+    assert_response :success
+  end
+
+end
