15c2ec5ef9ae581b4a076b4b90fc333e4fd3b054
Hiroki Kumamoto <kumabook@live.jp>
2017-08-03 01:14:29 +0900

    Add introduce_new_ios_app mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,14 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
-    mail(:to => user.email,
-         :subject => "Your password has been reset")
+    mail(to:      user.email,
+         subject: "Your password has been reset")
+  end
+
+  def introduce_new_ios_app
+    subject = 'Introducing new StickyNotes iOS App'
+    mail(to:      StickynotesBackend::Application.config.sender_email,
+         bcc:     User.all.map {|user| user.email },
+         subject: subject)
   end
 end
