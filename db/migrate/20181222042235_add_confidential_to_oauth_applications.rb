4a46ebb75d96ff7fe8efba57b14be928dd673cc2
Hiroki Kumamoto <kumabook@live.jp>
2018-12-22 13:38:44 +0900

    `rails g migration add_confidential_to_oauth_applications`

---
+++ db/migrate/20181222042235_add_confidential_to_oauth_applications.rb
@@ -0,0 +1,5 @@
+class AddConfidentialToOauthApplications < ActiveRecord::Migration[5.0]
+  def change
+    add_column :oauth_applications, :confidential, :boolean, default: true, null: false
+  end
+end
