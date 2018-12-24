c32cd0f8b26b850fdf0b6b6063e6ff9d3694f7c3
Hiroki Kumamoto <kumabook@live.jp>
2018-12-24 14:52:33 +0900

    Change log file path to `/var/log/app.log`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  config.logger = Logger.new('/var/log/app.log', 14, 10 * 1024 * 1024)
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :info
